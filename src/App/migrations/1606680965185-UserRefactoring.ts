43813ffb9cd319a980b630ab7b67aed8d2c00dc1
Gentrit Abazi <gentritabazi01@gmail.com>
2020-12-06 21:01:07 +0100

    Formatted and added auto-increment

--- src/App/migrations/1606680965185-UserRefactoring.ts
+++ src/App/migrations/1606680965185-UserRefactoring.ts
@@ -4,34 +4,36 @@ export class UserRefactoring1606680965185 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(new Table({
-            name: "users",
+            name: 'users',
             columns: [
                 {
-                    name: "id",
-                    type: "int",
-                    isPrimary: true
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
                 },
                 {
-                    name: "first_name",
-                    type: "varchar",
+                    name: 'first_name',
+                    type: 'varchar',
                 },
                 {
-                    name: "last_name",
-                    type: "varchar",
+                    name: 'last_name',
+                    type: 'varchar',
                 },
                 {
-                    name: "email",
-                    type: "varchar",
+                    name: 'email',
+                    type: 'varchar',
                 },
                 {
-                    name: "password",
-                    type: "varchar",
+                    name: 'password',
+                    type: 'varchar',
                 },
                 {
-                    name: "status",
-                    type: "enum",
-                    enum: ["active", "inactive", "block"],
-                    enumName: "statusEnum",
+                    name: 'status',
+                    type: 'enum',
+                    enum: ['active', 'inactive', 'block'],
+                    enumName: 'statusEnum',
                     default: '"active"'
                 }
             ]
