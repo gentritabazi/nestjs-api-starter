cbb07d07ae53f995bd003fa3e84ff9d34ca08def
Gentrit Abazi <gentritabazi01@gmail.com>
2020-12-13 14:29:28 +0100

    Added method validatePassword

--- src/Users/entities/user.entity.ts
+++ src/Users/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { EntityBase } from '../../App/abstracts/entity.base';
+import * as bcrypt from 'bcrypt';
 
 @Entity('users')
 export class User extends EntityBase {
@@ -15,9 +16,13 @@ export class User extends EntityBase {
   @Column()
   email: string;
 
-  @Column({select: false})
+  @Column({ select: false })
   password: string;
 
   @Column({ type: 'enum', enum: ['active', 'inactive', 'block'], default: 'active' })
   status: string;
+
+  async validatePassword(password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password);
+  }
 }
