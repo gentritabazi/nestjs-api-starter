df50d249bdfb266a3263c579f6efff826ac68497
Gentrit Abazi <gentritabazi01@gmail.com>
2020-12-16 20:20:11 +0100

    Removed unused methods.

--- src/Users/subscribers/user.subscriber.ts
+++ src/Users/subscribers/user.subscriber.ts
@@ -13,54 +13,10 @@ export class UserSubscriber implements EntitySubscriberInterface<User> {
   }
 
   /**
-   * Called after user is loaded.
-   */
-  afterLoad(entity: User) {
-    // console.log(`AFTER USER LOADED: `, entity);
-  }
-
-  /**
    * Called before user insertion.
    */
   async beforeInsert(event: InsertEvent<User>) {
-    // console.log(`BEFORE USER INSERTED: `, event.entity);
-
     event.entity.password = await this.hashPassword(event.entity.password);
-  }
-
-  /**
-   * Called after user insertion.
-   */
-  async afterInsert(event: InsertEvent<User>) {
-    // console.log(`AFTER USER INSERTED: `, event.entity);
-  }
-
-  /**
-   * Called before user update.
-   */
-  beforeUpdate(event: UpdateEvent<User>) {
-    // console.log(`BEFORE USER UPDATED: `, event.entity);
-  }
-
-  /**
-   * Called after user update.
-   */
-  afterUpdate(event: UpdateEvent<User>) {
-    // console.log(`AFTER USER UPDATED: `, event.entity);
-  }
-
-  /**
-   * Called before user removal.
-   */
-  beforeRemove(event: RemoveEvent<User>) {
-    // console.log(`BEFORE USER WITH ID ${event.entityId} REMOVED: `, event.entity);
-  }
-
-  /**
-   * Called after user removal.
-   */
-  afterRemove(event: RemoveEvent<User>) {
-    // console.log(`AFTER USER WITH ID ${event.entityId} REMOVED: `, event.entity);
   }
 
   /**
